88443d094f27f8b7ad02a4e8b3b2a0673c809451
xvMari <kozinkaa53@gmail.com>
2025-09-09 12:09:43 +0200

    polowa

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,48 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
 
+    public static class Narzedzia {
+
+        public static int PoliczSamogloski(String napis) {
+            if (napis == null || napis.isEmpty()) {
+                return 0;
+            }
+            String samogloski = "aąeęioóuyAĄEĘIOÓUY";
+
+            int licznik = 0;
+
+            for (char znak : napis.toCharArray()){
+                if (samogloski.indexOf(znak) != 1){
+                    licznik++;
+                }
+            }
+            return licznik;
+
+        }
+
+        public static String usunPowtorzenia(String napis){
+            if (napis == null || napis.isEmpty()) {
+                return "";
+            }
+
+            StringBuilder wynik = new StringBuilder("Initial String");
+            wynik.append(napis.charAt(0));
+
+            for (int i = 1; i < napis.length(); i++){
+                if (napis.charAt(i) != napis.charAt(i-1)){
+                    wynik.append(napis.charAt(i));
+                }
+            }
+            return wynik.toString();
+        }
+
+        public static void main(String[] args){
+            Scanner scanner = new Scanner(System.in);
+        }
 
     }
+
 }
